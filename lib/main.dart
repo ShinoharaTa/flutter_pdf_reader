28a423dc6c19f6b3ca1fae2ff044fdcdaba41d11
tshinohara <takuma1201@gmail.com>
2024-11-28 20:07:38 +0900

    ダークテーマ対応

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,46 @@ void main() {
   runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.system; // 初期状態はシステムに追従
+
+  void _toggleThemeMode() {
+    setState(() {
+      _themeMode =
+          _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PDF Viewer with Library',
       theme: ThemeData(
+        brightness: Brightness.light,
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+        buttonTheme: ButtonThemeData(buttonColor: Colors.blue),
       ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+        ),
+        buttonTheme: ButtonThemeData(buttonColor: Colors.grey[800]),
+      ),
+      themeMode: _themeMode, // 現在のテーマモード
       home: LibraryScreen(),
     );
   }
