809dab76676ed6e27cbb3ea58c4596eb42b06d21
tshinohara <takuma1201@gmail.com>
2024-12-08 03:21:51 +0900

    ダークテーマ修正

--- lib/screens/viewer.dart
+++ lib/screens/viewer.dart
@@ -123,209 +123,221 @@ class _PDFViewerPageState extends ConsumerState<PDFViewerPage> {
     final fileStoreNotifier = ref.read(fileStoreProvider.notifier);
     final isFavorite = fileStore.favoriteFiles.contains(widget.filePath);
 
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Theme(
-        data: darkTheme,
-        child: SafeArea(
-          child: KeyboardListener(
-            focusNode: FocusNode(),
-            onKeyEvent: _handleKeyEvent, // キーボード操作を検知
-            child: Stack(children: [
-              GestureDetector(
-                onTap: _toggleOverlay,
-                child: PdfView(
-                  controller: _pdfController!,
-                  scrollDirection: scrollDirection,
-                  onPageChanged: (page) {
-                    setState(() {
-                      currentPage = page;
-                      _pageController.text =
-                          currentPage.toString(); // テキストボックスを更新
-                    });
-                  },
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: Colors.black,
+        systemNavigationBarIconBrightness: Brightness.light,
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Theme(
+          data: darkTheme,
+          child: SafeArea(
+            child: KeyboardListener(
+              focusNode: FocusNode(),
+              onKeyEvent: _handleKeyEvent, // キーボード操作を検知
+              child: Stack(children: [
+                GestureDetector(
+                  onTap: _toggleOverlay,
+                  child: PdfView(
+                    controller: _pdfController!,
+                    scrollDirection: scrollDirection,
+                    onPageChanged: (page) {
+                      setState(() {
+                        currentPage = page;
+                        _pageController.text =
+                            currentPage.toString(); // テキストボックスを更新
+                      });
+                    },
+                  ),
                 ),
-              ),
-              // 右上のメニューボタン
-              Positioned(
-                top: 16,
-                right: 16,
-                child: IconButton(
-                  icon: const Icon(Icons.menu),
-                  onPressed: _toggleOverlay,
-                ),
-              ),
-
-              // 右上のメニューボタン
-              // Positioned(
-              //   bottom: 16,
-              //   right: 32,
-              //   child: TextButton(
-              //     onPressed: _toggleOverlay,
-              //     child: Text("$currentPage / $totalPages"),
-              //   ),
-              // ),
-
-              // 上部バー（オーバーレイ）
-              if (_isOverlayVisible)
+                // 右上のメニューボタン
                 Positioned(
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  child: AnimatedOpacity(
-                    opacity: _isOverlayVisible ? 1.0 : 0.0,
-                    duration: const Duration(milliseconds: 500),
-                    child: Container(
-                      color: Colors.black.withOpacity(0.8),
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 8, horizontal: 16),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.arrow_back),
-                            onPressed: () async {
-                              await ref
-                                  .read(fileStoreProvider.notifier)
-                                  .updatePDFState(
-                                    widget.filePath,
-                                    lastPage: currentPage,
-                                    totalPages: totalPages,
-                                    isRead: currentPage == totalPages,
-                                  );
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                          Text(
-                            widget.filePath.split("/").last,
-                            style: const TextStyle(fontSize: 18),
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.close),
-                            onPressed: _toggleOverlay,
-                          ),
-                        ],
+                  top: 16,
+                  right: 16,
+                  child: IconButton(
+                    icon: const Icon(Icons.menu),
+                    onPressed: _toggleOverlay,
+                  ),
+                ),
+
+                // 右上のメニューボタン
+                // Positioned(
+                //   bottom: 16,
+                //   right: 32,
+                //   child: TextButton(
+                //     onPressed: _toggleOverlay,
+                //     child: Text("$currentPage / $totalPages"),
+                //   ),
+                // ),
+
+                // 上部バー（オーバーレイ）
+                if (_isOverlayVisible)
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: AnimatedOpacity(
+                      opacity: _isOverlayVisible ? 1.0 : 0.0,
+                      duration: const Duration(milliseconds: 500),
+                      child: Container(
+                        color: Colors.black.withOpacity(0.8),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8, horizontal: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.arrow_back),
+                              onPressed: () async {
+                                await ref
+                                    .read(fileStoreProvider.notifier)
+                                    .updatePDFState(
+                                      widget.filePath,
+                                      lastPage: currentPage,
+                                      totalPages: totalPages,
+                                      isRead: currentPage == totalPages,
+                                    );
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            Text(
+                              widget.filePath.split("/").last,
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.close),
+                              onPressed: _toggleOverlay,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              // 下部オーバーレイ（スクロール方向設定）
-              if (_isOverlayVisible)
-                Positioned(
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  child: AnimatedOpacity(
-                    opacity: _isOverlayVisible ? 1.0 : 0.0,
-                    duration: const Duration(milliseconds: 500),
-                    child: Container(
-                      color: Colors.black.withOpacity(0.8),
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 16, horizontal: 16),
-                      child: Center(
-                        child: ConstrainedBox(
-                          constraints: const BoxConstraints(maxWidth: 420),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  IconButton(
-                                    icon: Icon(
-                                      settings.scrollDirection ==
-                                              Axis.horizontal
-                                          ? Icons.swap_horiz_rounded
-                                          : Icons.swap_vert_rounded,
-                                      size: 36.0,
-                                    ),
-                                    onPressed: () => settingsNotifier
-                                        .toggleScrollDirection(),
-                                  ),
-                                  IconButton(
-                                    icon: Icon(
-                                      isFavorite
-                                          ? Icons.star_rounded
-                                          : Icons.star_border_rounded,
-                                      color: isFavorite
-                                          ? Colors.yellow
-                                          : Colors.white70,
-                                      size: 36.0,
-                                    ),
-                                    onPressed: () async {
-                                      if (isFavorite) {
-                                        await fileStoreNotifier
-                                            .removeFavoriteFile(
-                                                widget.filePath);
-                                      } else {
-                                        await fileStoreNotifier
-                                            .addFavoriteFile(widget.filePath);
-                                      }
-                                    },
-                                  ),
-                                ],
-                              ),
-                              Slider(
-                                value: currentPage.toDouble(),
-                                max: totalPages.toDouble(),
-                                divisions: totalPages - 1,
-                                onChanged: (double value) {
-                                  _goToPage(value.toInt());
-                                },
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  IconButton(
-                                    icon: const Icon(
-                                      Icons.navigate_before_rounded,
-                                      size: 36.0,
-                                    ),
-                                    onPressed: () => _goToPage(currentPage - 1),
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      const Text(
-                                        "Page: ",
-                                        style: TextStyle(fontSize: 16),
+                // 下部オーバーレイ（スクロール方向設定）
+                if (_isOverlayVisible)
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: AnimatedOpacity(
+                      opacity: _isOverlayVisible ? 1.0 : 0.0,
+                      duration: const Duration(milliseconds: 500),
+                      child: Container(
+                        color: Colors.black.withOpacity(0.8),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 16, horizontal: 16),
+                        child: Center(
+                          child: ConstrainedBox(
+                            constraints: const BoxConstraints(maxWidth: 420),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    IconButton(
+                                      icon: Icon(
+                                        settings.scrollDirection ==
+                                                Axis.horizontal
+                                            ? Icons.swap_horiz_rounded
+                                            : Icons.swap_vert_rounded,
+                                        size: 36.0,
                                       ),
-                                      SizedBox(
-                                        width: 48,
-                                        child: TextField(
-                                          controller: _pageController,
-                                          keyboardType: TextInputType.number,
-                                          textAlign: TextAlign.center,
-                                          onSubmitted: (value) =>
-                                              _goToPage(int.tryParse(value)),
+                                      onPressed: () => settingsNotifier
+                                          .toggleScrollDirection(),
+                                    ),
+                                    IconButton(
+                                      icon: Icon(
+                                        isFavorite
+                                            ? Icons.star_rounded
+                                            : Icons.star_border_rounded,
+                                        color: isFavorite
+                                            ? Colors.yellow
+                                            : Colors.white70,
+                                        size: 36.0,
+                                      ),
+                                      onPressed: () async {
+                                        if (isFavorite) {
+                                          await fileStoreNotifier
+                                              .removeFavoriteFile(
+                                                  widget.filePath);
+                                        } else {
+                                          await fileStoreNotifier
+                                              .addFavoriteFile(widget.filePath);
+                                        }
+                                      },
+                                    ),
+                                  ],
+                                ),
+                                Slider(
+                                  value: currentPage.toDouble(),
+                                  max: totalPages.toDouble(),
+                                  divisions: totalPages - 1,
+                                  onChanged: (double value) {
+                                    _goToPage(value.toInt());
+                                  },
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    IconButton(
+                                      icon: const Icon(
+                                        Icons.navigate_before_rounded,
+                                        size: 36.0,
+                                      ),
+                                      onPressed: () =>
+                                          _goToPage(currentPage - 1),
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        const Text(
+                                          "Page: ",
+                                          style: TextStyle(fontSize: 16),
+                                        ),
+                                        SizedBox(
+                                          width: 48,
+                                          child: TextField(
+                                            controller: _pageController,
+                                            keyboardType: TextInputType.number,
+                                            textAlign: TextAlign.center,
+                                            onSubmitted: (value) =>
+                                                _goToPage(int.tryParse(value)),
+                                            style:
+                                                const TextStyle(fontSize: 16),
+                                          ),
+                                        ),
+                                        Text(
+                                          "/ $totalPages",
                                           style: const TextStyle(fontSize: 16),
                                         ),
-                                      ),
-                                      Text(
-                                        "/ $totalPages",
-                                        style: const TextStyle(fontSize: 16),
-                                      ),
-                                    ],
-                                  ),
-                                  IconButton(
-                                    icon: const Icon(
-                                      Icons.navigate_next_rounded,
-                                      size: 36.0,
+                                      ],
                                     ),
-                                    onPressed: () => _goToPage(currentPage + 1),
-                                  ),
-                                ],
-                              ),
-                            ],
+                                    IconButton(
+                                      icon: const Icon(
+                                        Icons.navigate_next_rounded,
+                                        size: 36.0,
+                                      ),
+                                      onPressed: () =>
+                                          _goToPage(currentPage + 1),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-            ]),
+              ]),
+            ),
           ),
         ),
       ),
